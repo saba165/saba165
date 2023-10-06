c323a39ed0d3380713ab20dad884fc1d53ae76bf
saba165 <147132687+saba165@users.noreply.github.com>
2023-10-06 00:36:05 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(){
+  return(
+    <>      
+          <div className='card'>
+              <img src='image.jpg' alt='networkImage'/>
+              <div className='content'>
+                  <h1 className='title'>Swat</h1>
+                  <p> Swat district also known as....</p>
+                  <p>
+                  In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. 
+
+                  </p>
+
+              </div>
+          </div>
+    </>
+  );
+}
+
+export default App
